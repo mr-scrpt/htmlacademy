e55e6034d4b44be8447f6112d760714b11626715
mr-scrpt <mr.scrpt@gmail.com>
2022-04-02 15:12:46 +0300

    fix

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,6 +1,7 @@
 import PageMain from '../page-main/page-main';
 import { appPropType } from '../..';
 
+
 function App({countSnippet}: appPropType): JSX.Element {
   return <PageMain countSnippet={countSnippet}/>;
 }
